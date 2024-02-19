ea361f08f6ff105736f522f11b9dc54a75b8ebe3
Jngnzl28 <jngnzles@gmail.com>
2024-02-19 10:37:31 -0800

    Lab3

---
+++ Labs/Lab3/Lab3_CF.cpp
@@ -0,0 +1,23 @@
+//Lab2_calc.cpp
+/***
+ * { This program creates numbers and displays them}
+***/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int y = 5;
+
+    while(x < y) 
+    {
+        printf("%i\n", x);
+        x++;
+    }
+    printf("Looped through 5 times\n");
+    return 0;
+}
