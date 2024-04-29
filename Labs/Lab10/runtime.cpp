629225842a1339ee1f0724f185510f51779f8176
Jngnzl28 <jngnzles@gmail.com>
2024-04-28 21:26:10 -0700

    Lab10 Start

---
+++ Labs/Lab10/runtime.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+
+int iterativeSearch(vector<int>v, int elem)
+{
+    for(int i = 0; i < v.size(); i++)
+    {
+        if(v[i] == elem)
+            return i;
+    }
+    return -1;
+}
+
+int binarySearch(vector<int>v, int start, int end, int elem)
+{
+    if (start > end)
+        return -1;
+    int mid = (start + end) / 2;
+    if (v[mid] > elem){
+        end = mid - 1;
+    } else if (v[mid] < elem) {
+        start = mid + 1;
+    } else {
+        return mid;
+    }
+    return binarySearch(v, start, end, elem);
+}
+
+void vecGen(string filename, vector<int> & v)
+{
+    ifstream file(filename);
+    int num;
+    v.clear();
+    while (file.is_open() && file >> num) {
+        v.push_back(num);
+    }
+    file.close();
+}
+
+int main()
+{
+    vector<int> v;
+    vecGen("10000_numbers.csv", v);
+
+    vector<int> elem_to_find;
+    vecGen("test_elem.csv", elem_to_find);
+
+    for(int i = 0; i < elem_to_find.size(); i++)
+    {
+        int elem = elem_to_find[i];
+
+        auto start = chrono::high_resolution_clock::now();
+        int index_if_found = iterativeSearch(v, elem);
+        auto end = chrono::high_resolution_clock::now();
+
+        auto duration = chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+        cout << index_if_found << ": " << duration.count() << endl;
+    }
+
+    cout << endl;
+
+    int b_start = 0;
+    int b_end = v.size() - 1;
+
+    for(int i = 0; i < elem_to_find.size(); i++)
+    {
+        int elem = elem_to_find[i];
+
+        auto start = chrono::high_resolution_clock::now();
+        int index_if_found = binarySearch(v, b_start, b_end, elem);
+        auto end = chrono::high_resolution_clock::now();
+
+        auto duration = chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+        cout << index_if_found << ": " << duration.count() << endl;
+    }
+    
+    return 0;
+}
+
